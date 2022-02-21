cbaa6ae083f30dd716e930e75aad878baaffc4a7
Timur Gayazov <timurgayazovfw@gmail.com>
2022-02-21 15:23:02 +0000

    works

--- parallel2.cpp
+++ parallel2.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void print(double**& A, int size){
+void print(double** A, int size){
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++)
             cout << A[i][j] << ' ';
@@ -31,9 +31,6 @@ int main(int argc, char *argv[]){
     A[size+1][0] = Q21;
     A[size+1][size+1] = Q22;
 
-    
-#pragma acc parallel
-{
     for (int i = 1; i < size + 1; i++){
         A[0][i] = A[0][i-1] + step;
         A[size + 1][i] = A[size + 1][i-1] + step;
@@ -43,48 +40,44 @@ int main(int argc, char *argv[]){
         A[j][0] = A[j-1][0] + step;
         A[j][size + 1] = A[j-1][size + 1] + step;
     }
-}
 
     for (int i = 1; i < size + 1; i++)
         for (int j = 1; j < size + 1; j++)
             A[i][j] = 0;
 
-    double Anew[size+2][size+2];
-
-#pragma acc loop independent collapse(2)
-    for (int i = 0; i < size + 2; i++)
-        for (int j = 0; j < size + 2; j++)
-            Anew[i][j] = A[i][j];
-
     // end
+
+    // for (int i = 0; i < size + 2; i++){
+    //     for (int j = 0; j < size + 2; j++)
+    //         cout << A[i][j] << ' ';
+    //     cout << endl;
+    // }
     
-    
+    double Anew[size+2][size+2];
     int iter = 0;
     double err = 1;
 
-    #pragma acc data copy(Anew,A) create(err) 
+    #pragma acc data copy(A) create(Anew)
     {
     while ((err > accuracy) && (iter < iters)){
         iter++;
-        #pragma acc kernels
-        {
         err = 0;
-        #pragma acc loop independent collapse(2)
-        for (int j = 1; j < size + 1; j++)
-            for (int i = 1; i < size + 1; i++)
-                Anew[i][j] = 0.25 * (A[i+1][j] + A[i-1][j] + A[i][j-1] + A[i][j+1]);
 
+        #pragma acc kernels
+    {
         #pragma acc loop independent collapse(2) reduction(max:err)
         for (int j = 1; j < size + 1; j++)
             for (int i = 1; i < size + 1; i++){
-                A[i][j] = 0.25 * (Anew[i+1][j] + Anew[i-1][j] + Anew[i][j-1] + Anew[i][j+1]);
+                Anew[i][j] = 0.25 * (A[i+1][j] + A[i-1][j] + A[i][j-1] + A[i][j+1]);
                 err = max(err, Anew[i][j] - A[i][j]);
             }
-        
-        }
+        #pragma acc loop independent collapse(2)
+        for (int i = 1; i < size + 1; i++)
+            for (int j = 1; j < size + 1; j++)
+                A[i][j] = Anew[i][j];
+    }
 
         if ((iter % 100 == 0) || (iter == 1))
-            #pragma acc update self(err)
             cout << iter << ' ' << err << endl;
     }
     }
