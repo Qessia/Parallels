28a6c553ac5f6c7c58d4dc8d624486332da0aeee
Timur <52119664+Qessia@users.noreply.github.com>
2022-03-15 11:47:16 +0700

    Add files via upload

--- parallel2.cpp
+++ parallel2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     double accuracy = atof(argv[1]); //argv[1] - accuracy
-    int size = strtol(argv[2], NULL, 10); //argv[2] - size
+    int size = strtol(argv[2], NULL, 10) + 2; //argv[2] - size
     int iters = strtol(argv[3], NULL, 10); //argv[3] - iters
 
     // initializing matrix
@@ -15,41 +15,42 @@ int main(int argc, char *argv[]){
     double Q21 = 20.;
     double Q22 = 30.;
 
-    double step = 10. / (size + 2);
+    double step = 10. / size;
 
-    double A[size+2][size+2];
+    double A[size][size];
 
     A[0][0] = Q11;
-    A[0][size+1] = Q12;
-    A[size+1][0] = Q21;
-    A[size+1][size+1] = Q22;
+    A[0][size-1] = Q12;
+    A[size-1][0] = Q21;
+    A[size-1][size-1] = Q22;
 
     #pragma acc parallel // using pragma parallel while fulfilling initial matrix
     {
-    for (int i = 1; i < size + 1; i++){
+    for (int i = 1; i < size-1; i++){
         A[0][i] = A[0][i-1] + step;
-        A[size + 1][i] = A[size + 1][i-1] + step;
+        A[size - 1][i] = A[size - 1][i-1] + step;
     }
 
-    for (int j = 1; j < size + 1; j++){
+    for (int j = 1; j < size - 1; j++){
         A[j][0] = A[j-1][0] + step;
-        A[j][size + 1] = A[j-1][size + 1] + step;
+        A[j][size - 1] = A[j-1][size - 1] + step;
     }
 
-    for (int i = 1; i < size + 1; i++)
-        for (int j = 1; j < size + 1; j++)
+    for (int i = 1; i < size - 1; i++)
+        for (int j = 1; j < size - 1; j++)
             A[i][j] = 0;
 
     }
     
-    double Anew[size+2][size+2];
+    double Anew[size][size];
     int iter = 0;
     double err = 1;
 
     cublasHandle_t handle;
     cublasCreate(&handle);
+    double temp[size*size];
 
-    #pragma acc data copy(A) create(Anew, err) // here we copy array A to GPU and create Anew, err on GPU
+    #pragma acc data copy(A) create(Anew, err, temp) // here we copy array A to GPU and create Anew, err on GPU
     {
     while ((err > accuracy) && (iter < iters)){
         iter++;
@@ -59,36 +60,38 @@ int main(int argc, char *argv[]){
             {
             err = 0;
                       
-            for (int j = 1; j < size + 1; j++)
-                for (int i = 1; i < size + 1; i++)
+            for (int j = 1; j < size - 1; j++)
+                for (int i = 1; i < size - 1; i++)
                     Anew[i][j] = 0.25 * (A[i+1][j] + A[i-1][j] + A[i][j-1] + A[i][j+1]);
                     //err = max(err, Anew[i][j] - A[i][j]);
-
-            double temp[size+2][size+2];
+            }
+            #pragma acc wait(1)
+            int idx;
             #pragma acc host_data use_device(Anew, A, temp)
             {
             double alpha = -1.;
-            cublasDcopy_v2(handle, ((size+2)*(size+2)), *Anew, 1, *temp, 1);
-            cublasDaxpy_v2(handle, ((size+2)*(size+2)), &alpha, *A, 1, *temp, 1);
-            int idx;
-            cublasIdamax(handle, ((size+2)*(size+2)), *temp, 1, &idx);
+            for (int i = 0; i < size; i++){
+                cublasDcopy_v2(handle, size, Anew[i], 1, &temp[i*size], 1);
+                cublasDaxpy_v2(handle, size, &alpha, A[i], 1, &temp[i*size], 1);
+            }
+            
+            cublasIdamax_v2(handle, size*size, temp, 1, &idx);
             }
             #pragma acc update self(temp)
-            err = temp[(idx-1)/(size+2)][(idx-1)%(size+2)];
-            }
+            err = temp[idx-1];
         } else{
             #pragma acc kernels async(1)
             #pragma acc loop independent collapse(2)
-            for (int j = 1; j < size + 1; j++)
-                for (int i = 1; i < size + 1; i++){
+            for (int j = 1; j < size - 1; j++)
+                for (int i = 1; i < size - 1; i++){
                     Anew[i][j] = 0.25 * (A[i+1][j] + A[i-1][j] + A[i][j-1] + A[i][j+1]);
                     
                 }
 
         }
         #pragma acc kernels async(1) // updating matrix
-        for (int i = 1; i < size + 1; i++)
-            for (int j = 1; j < size + 1; j++)
+        for (int i = 1; i < size - 1; i++)
+            for (int j = 1; j < size - 1; j++)
                 A[i][j] = Anew[i][j];
 
         if ((iter % 100 == 0) || (iter == 1)){ // every 100 iterations:
@@ -101,7 +104,7 @@ int main(int argc, char *argv[]){
 
     cout << iter << ' ' << err << endl;
     cublasDestroy(handle);
-    cout << "HELLO";
+    cout << "Made with CuBLAS";
 
     return 0;
 }
